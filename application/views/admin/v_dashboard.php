e25e1b513178f2ab133ea7934eb9e705818809ae
WilmanTb <rizki02tebe@gmail.com>
2024-07-20 19:12:26 +0700

    update komentar & inbox

--- application/views/admin/v_dashboard.php
+++ application/views/admin/v_dashboard.php
@@ -5,6 +5,7 @@ error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,208 +26,208 @@ error_reporting(0);
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="<?php echo base_url() . 'assets/dist/css/skins/_all-skins.min.css' ?>">
   <?php
-/* Mengambil query report*/
-foreach ($visitor as $result) {
+  /* Mengambil query report*/
+  foreach ($visitor as $result) {
     $bulan[] = $result->tgl; //ambil bulan
     $value[] = (float) $result->jumlah; //ambil nilai
-}
-/* end mengambil query*/
+  }
+  /* end mengambil query*/
 
-?>
+  ?>
 
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!--Header-->
-  <?php
-$this->load->view('admin/v_header');
-?>
+    <!--Header-->
+    <?php
+    $this->load->view('admin/v_header');
+    ?>
 
-<?php
-$page = array (
-  "page" => "dashboard"
-);
-$this->load->view('admin/v_sidebar', $page);
-?>
+    <?php
+    $page = array(
+      "page" => "dashboard"
+    );
+    $this->load->view('admin/v_sidebar', $page);
+    ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1 style="text-align:center; font-family:square721 bt;">
-       <br><b>DASHBOARD ADMIN </b> <br>
-     <small></small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
-    </section>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1 style="text-align:center; font-family:square721 bt;">
+          <br><b>DASHBOARD ADMIN </b> <br>
+          <small></small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Dashboard</li>
+        </ol>
+      </section>
 
-    <!-- Main content -->
-    <section class="content">
-      <!-- Info boxes -->
-      <div class="row">
-        <div class="col-md-4 col-sm-7 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-chrome"></i></span>
+      <!-- Main content -->
+      <section class="content">
+        <!-- Info boxes -->
+        <div class="row">
+          <div class="col-md-4 col-sm-7 col-xs-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-aqua"><i class="fa fa-chrome"></i></span>
               <?php
-$query = $this->db->query("SELECT * FROM tbl_pengunjung WHERE pengunjung_perangkat='Chrome'");
-$jml = $query->num_rows();
-?>
-            <div class="info-box-content">
-              <span class="info-box-text">Chrome</span>
-              <span class="info-box-number"><?php echo $jml; ?></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-4 col-sm-7 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-firefox"></i></span>
-            <?php
-$query = $this->db->query("SELECT * FROM tbl_pengunjung WHERE pengunjung_perangkat='Firefox' OR pengunjung_perangkat='Mozilla'");
-$jml = $query->num_rows();
-?>
-            <div class="info-box-content">
-              <span class="info-box-text">Mozilla Firefox</span>
-              <span class="info-box-number"><?php echo $jml; ?></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-
-        <!-- /.col -->
-        <div class="col-md-4 col-sm-7 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-opera"></i></span>
-            <?php
-$query = $this->db->query("SELECT * FROM tbl_pengunjung WHERE pengunjung_perangkat='Opera'");
-$jml = $query->num_rows();
-?>
-            <div class="info-box-content">
-              <span class="info-box-text">Opera</span>
-              <span class="info-box-number"><?php echo $jml; ?></span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-
-      <div class="row">
-        <div class="col-md-12">
-          <div class="box">
-            <div class="box-header with-border">
-              <h3 class="box-title">Pengunjung bulan ini</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <div class="row">
-                <div class="col-md-12">
-
-                  <div class="col-md-12">
-                          <canvas id="canvas" width="1000" height="280"></canvas>
-                  </div>
-                  <!-- /.chart-responsive -->
-                </div>
-                <!-- /.col -->
-
-                <!-- /.col -->
+              $query = $this->db->query("SELECT * FROM tbl_pengunjung WHERE pengunjung_perangkat='Chrome'");
+              $jml = $query->num_rows();
+              ?>
+              <div class="info-box-content">
+                <span class="info-box-text">Chrome</span>
+                <span class="info-box-number"><?php echo $jml; ?></span>
               </div>
-              <!-- /.row -->
+              <!-- /.info-box-content -->
             </div>
-            <!-- ./box-body -->
-
-            <!-- /.box-footer -->
+            <!-- /.info-box -->
           </div>
-          <!-- /.box -->
-        </div>
-        <!-- /.col -->
-      </div>
+          <!-- /.col -->
+          <div class="col-md-4 col-sm-7 col-xs-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-red"><i class="fa fa-firefox"></i></span>
+              <?php
+              $query = $this->db->query("SELECT * FROM tbl_pengunjung WHERE pengunjung_perangkat='Firefox' OR pengunjung_perangkat='Mozilla'");
+              $jml = $query->num_rows();
+              ?>
+              <div class="info-box-content">
+                <span class="info-box-text">Mozilla Firefox</span>
+                <span class="info-box-number"><?php echo $jml; ?></span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
 
-    </section>
+          <!-- /.col -->
+          <div class="col-md-4 col-sm-7 col-xs-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-green"><i class="fa fa-opera"></i></span>
+              <?php
+              $query = $this->db->query("SELECT * FROM tbl_pengunjung WHERE pengunjung_perangkat='Opera'");
+              $jml = $query->num_rows();
+              ?>
+              <div class="info-box-content">
+                <span class="info-box-text">Opera</span>
+                <span class="info-box-number"><?php echo $jml; ?></span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Pengunjung bulan ini</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                <div class="row">
+                  <div class="col-md-12">
+
+                    <div class="col-md-12">
+                      <canvas id="canvas" width="1000" height="280"></canvas>
+                    </div>
+                    <!-- /.chart-responsive -->
+                  </div>
+                  <!-- /.col -->
+
+                  <!-- /.col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <!-- ./box-body -->
+
+              <!-- /.box-footer -->
+            </div>
+            <!-- /.box -->
+          </div>
+          <!-- /.col -->
+        </div>
+
+      </section>
+    </div>
+
+    <?php
+    $this->load->view('admin/v_footer');
+    ?>
+
   </div>
 
-<?php
-$this->load->view('admin/v_footer');
-?>
+  <!-- ./wrapper -->
 
-</div>
+  <!-- jQuery 2.2.3 -->
+  <script src="<?php echo base_url() . 'assets/plugins/jQuery/jquery-2.2.3.min.js' ?>"></script>
+  <!-- Bootstrap 3.3.6 -->
+  <script src="<?php echo base_url() . 'assets/bootstrap/js/bootstrap.min.js' ?>"></script>
+  <!-- FastClick -->
+  <script src="<?php echo base_url() . 'assets/plugins/fastclick/fastclick.js' ?>"></script>
+  <!-- AdminLTE App -->
+  <script src="<?php echo base_url() . 'assets/dist/js/app.min.js' ?>"></script>
+  <!-- Sparkline -->
+  <script src="<?php echo base_url() . 'assets/plugins/sparkline/jquery.sparkline.min.js' ?>"></script>
+  <!-- jvectormap -->
+  <script src="<?php echo base_url() . 'assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js' ?>"></script>
+  <script src="<?php echo base_url() . 'assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js' ?>"></script>
+  <!-- SlimScroll 1.3.0 -->
+  <script src="<?php echo base_url() . 'assets/plugins/slimScroll/jquery.slimscroll.min.js' ?>"></script>
+  <!-- ChartJS 1.0.1 -->
+  <script src="<?php echo base_url() . 'assets/plugins/chartjs/Chart.min.js' ?>"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="<?php echo base_url() . 'assets/dist/js/pages/dashboard2.js' ?>"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="<?php echo base_url() . 'assets/dist/js/demo.js' ?>"></script>
 
-<!-- ./wrapper -->
+  <script>
+    var lineChartData = {
+      labels: <?php echo json_encode($bulan); ?>,
+      datasets: [
 
-<!-- jQuery 2.2.3 -->
-<script src="<?php echo base_url() . 'assets/plugins/jQuery/jquery-2.2.3.min.js' ?>"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="<?php echo base_url() . 'assets/bootstrap/js/bootstrap.min.js' ?>"></script>
-<!-- FastClick -->
-<script src="<?php echo base_url() . 'assets/plugins/fastclick/fastclick.js' ?>"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo base_url() . 'assets/dist/js/app.min.js' ?>"></script>
-<!-- Sparkline -->
-<script src="<?php echo base_url() . 'assets/plugins/sparkline/jquery.sparkline.min.js' ?>"></script>
-<!-- jvectormap -->
-<script src="<?php echo base_url() . 'assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js' ?>"></script>
-<script src="<?php echo base_url() . 'assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js' ?>"></script>
-<!-- SlimScroll 1.3.0 -->
-<script src="<?php echo base_url() . 'assets/plugins/slimScroll/jquery.slimscroll.min.js' ?>"></script>
-<!-- ChartJS 1.0.1 -->
-<script src="<?php echo base_url() . 'assets/plugins/chartjs/Chart.min.js' ?>"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url() . 'assets/dist/js/pages/dashboard2.js' ?>"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url() . 'assets/dist/js/demo.js' ?>"></script>
+        {
+          fillColor: "rgba(60,141,188,0.9)",
+          strokeColor: "rgba(60,141,188,0.8)",
+          pointColor: "#3b8bba",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(152,235,239,1)",
+          data: <?php echo json_encode($value); ?>
+        }
 
-<script>
+      ]
 
-            var lineChartData = {
-                labels : <?php echo json_encode($bulan); ?>,
-                datasets : [
+    }
 
-                    {
-                        fillColor: "rgba(60,141,188,0.9)",
-                        strokeColor: "rgba(60,141,188,0.8)",
-                        pointColor: "#3b8bba",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(152,235,239,1)",
-                        data : <?php echo json_encode($value); ?>
-                    }
+    var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
 
-                ]
-
-            }
-
-        var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
-
-        var canvas = new Chart(myLine).Line(lineChartData, {
-            scaleShowGridLines : true,
-            scaleGridLineColor : "rgba(0,0,0,.005)",
-            scaleGridLineWidth : 0,
-            scaleShowHorizontalLines: true,
-            scaleShowVerticalLines: true,
-            bezierCurve : true,
-            bezierCurveTension : 0.4,
-            pointDot : true,
-            pointDotRadius : 4,
-            pointDotStrokeWidth : 1,
-            pointHitDetectionRadius : 2,
-            datasetStroke : true,
-            tooltipCornerRadius: 2,
-            datasetStrokeWidth : 2,
-            datasetFill : true,
-            legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-            responsive: true
-        });
-
-        </script>
+    var canvas = new Chart(myLine).Line(lineChartData, {
+      scaleShowGridLines: true,
+      scaleGridLineColor: "rgba(0,0,0,.005)",
+      scaleGridLineWidth: 0,
+      scaleShowHorizontalLines: true,
+      scaleShowVerticalLines: true,
+      bezierCurve: true,
+      bezierCurveTension: 0.4,
+      pointDot: true,
+      pointDotRadius: 4,
+      pointDotStrokeWidth: 1,
+      pointHitDetectionRadius: 2,
+      datasetStroke: true,
+      tooltipCornerRadius: 2,
+      datasetStrokeWidth: 2,
+      datasetFill: true,
+      legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+      responsive: true
+    });
+  </script>
 
 </body>
+
 </html>
